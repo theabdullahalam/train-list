b79cbbe680c555039529a25b3e1c71d4ded642ca
Abdullah Alam <abdullahalam@protonmail.com>
2022-04-08 18:44:49 +0530

    add file checks

--- main.py
+++ main.py
@@ -18,23 +18,39 @@ def get_origin_divn(train_no):
     
     return 'Not Found'
 
-with open(FAX_SHEET, 'r') as f:
-    reader = csv.DictReader(f)
-    field_names = ['S.No.','Train no','Division','Class','Date','FROM','TO','PNR NO.','NAME &NO. OF BERTHS','REFERENCE','Remarks','MOB.NO.']
-    updated_rows = []
+def generate_updated_sheet():
+  with open(FAX_SHEET, 'r') as f:
+      reader = csv.DictReader(f)
+      field_names = ['S.No.','Train no','Division','Class','Date','FROM','TO','PNR NO.','NAME &NO. OF BERTHS','REFERENCE','Remarks','MOB.NO.']
+      updated_rows = []
 
-    for row in reader:
-      new_row = row
-      new_row['Division'] = get_origin_divn(row['Train no'])
+      for row in reader:
+        new_row = row
+        new_row['Division'] = get_origin_divn(row['Train no'])
 
-      # handle BCT case
-      if new_row['Division'] == 'BCT':
-        if new_row['Class'] == 'SL' or new_row['Class'] == '2SL':
-          new_row['Division'] = 'CCG'          
+        # handle BCT case
+        if new_row['Division'] == 'BCT':
+          if new_row['Class'] == 'SL' or new_row['Class'] == '2SL':
+            new_row['Division'] = 'CCG'          
 
-      updated_rows.append(new_row)
+        updated_rows.append(new_row)
 
-    with open(UPDATED_SHEET, 'w') as u:
-        writer = csv.DictWriter(u, fieldnames=field_names)
-        writer.writeheader()
-        writer.writerows(updated_rows)
+      with open(UPDATED_SHEET, 'w') as u:
+          writer = csv.DictWriter(u, fieldnames=field_names)
+          writer.writeheader()
+          writer.writerows(updated_rows)
+
+def check_files():
+  if not os.path.isfile(MOSR_FILE):
+    print_fancy('MOSR file not found')
+    sys.exit()
+
+def print_fancy(string):
+  print(f'\n--------------------\n{string}\n--------------------\n')
+
+# check if the required files are present
+check_files()
+
+# generate updated sheet
+generate_updated_sheet()
+print_fancy('Updated sheet generated')
